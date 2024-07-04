66cfb8dbb7e8b6131b7cc3b2be101c9225cecd2f
Malgorzata Podgorska <malgosia.podgorska100@gmail.com>
2024-07-04 11:02:16 +0200

    Creating a BMI calculator

---
+++ name-band-generator.py
@@ -0,0 +1,7 @@
+print("Welcome to our name band generator!")
+city = input("Tell us what city did you grew up in: ")
+pet = input("What is the name of your pet?: ")
+
+print("Your band name could be " + city + " " + pet)
+
+
