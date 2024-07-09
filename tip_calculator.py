e15f9dc8995c68aec0a2fe90da52f9b5d4551a65
Malgorzata Podgorska <malgosia.podgorska100@gmail.com>
2024-07-09 13:46:38 +0200

    Fixed the issues

--- tip_calculator.py
+++ tip_calculator.py
@@ -6,18 +6,16 @@ print("Welcome to the tip calculator!")
 
 bill =float(input("What was the total bill?: "))
 tip = int(input("What percentage would you like to leave? 10 , 12, or 15?: "))
-people =int(input("How many people to splitt the bill?: $ "))
-
-bill_with_tip = tip / 100 * bill + bill
-print(bill_with_tip)
+people =int(input("How many people to split the bill?: $ "))
 
 
-'''tip_as_percent = tip / 100
+tip_as_percent = tip / 100
 total_tip_amount = bill * tip_as_percent
 total_bill = bill + total_tip_amount
 bill_per_person = total_bill / people
-final_amount = round(bill_per_person, 2)'''
+final_amount = round(bill_per_person, 2)
+final_amount = "{:.2f}".format(bill_per_person)
 
-'''print(f"Each person should pay: ${final_amount}")'''
+print(f"Each person should pay: ${final_amount}")
 
 
